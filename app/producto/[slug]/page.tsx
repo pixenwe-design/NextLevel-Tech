68a63bcb29384a8cb7460d94203e2161386c4173
Marcos <mago.06109@gmail.com>
2026-07-19 14:10:10 -0300

    Fix local product detail module resolution

--- app/producto/[slug]/page.tsx
+++ app/producto/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import ProductDetail from "./product-detail";
+import ProductDetail from "@/app/producto/[slug]/product-detail-client";
 
 export default async function ProductPage({params}:{params:Promise<{slug:string}>}){
   const {slug}=await params;
