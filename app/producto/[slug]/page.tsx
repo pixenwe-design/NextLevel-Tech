fb906c25b967c8ed34d84fb109857d29b3930b7e
Marcos <mago.06109@gmail.com>
2026-07-19 14:16:12 -0300

    Fix product detail client import

--- app/producto/[slug]/page.tsx
+++ app/producto/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import ProductDetail from "@/app/producto/[slug]/product-detail-client";
+import ProductDetail from "./product-detail-client";
 
 export default async function ProductPage({params}:{params:Promise<{slug:string}>}){
   const {slug}=await params;
